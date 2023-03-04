bd230d2e81903657af1da392e8e7d6cd310cb6da
Rob <123372510+pezagna@users.noreply.github.com>
2023-03-04 17:35:06 -0300

    Refazendo o projeto com código mais limpo
    Refazendo o projeto com uma linha de código mais limpa.
    Adicionando arquivo main para trabalhar com o paradigma orientado a objetos.

--- Arquivo py/Projeto em grupo.py
+++ Arquivo py/Projeto em grupo.py
@@ -1,130 +1,47 @@
 import csv
 import datetime
-hora_atual = datetime.datetime.now()
-print(hora_atual)
 
-print("\n     Criar aquivo CSV\n")
-#a=(input("Digite o nome do arquivo: "))
-arquivo = open("./Arquivo.csv", "w", newline="", encoding="utf-8")
-adicionar = csv.writer(arquivo)
-resultado = []
-    # 3. grava as linhas
-for cont in range(0,1,1):
-    adicionar.writerow(["Idade", "Gênero", "Pergunta_1", "Pergunta_2", "Pergunta_3", "Pergunta_4"])        
-def entrevista():
-    import csv
-    from time import sleep
-    import datetime
-    hora_atual = datetime.datetime.now()
-    print(hora_atual)
+class Entrevista:
+    def __init__(self):
+        self.data = []
+        self.cabecalho = ["Idade", "Gênero", "Pergunta_1", "Pergunta_2", "Pergunta_3", "Pergunta_4","Data e Hora"]
 
-    print("\n     Criar aquivo CSV\n")
-    #a=(input("Digite o nome do arquivo: "))
-    arquivo = open("./Arquivo.csv", "a", newline="", encoding="utf-8")
-    
-    # 2. cria o objeto de gravação
-    adicionar = csv.writer(arquivo)
-    
-    # 3. grava as linhas
-    for cont in range(0,1,1):
-        adicionar.writerow(["Idade", "Gênero", "Pergunta_1", "Pergunta_2", "Pergunta_3", "Pergunta_4"])        
-        opcao = 0
-        while opcao !="00":
-            try:           
-                resultado.append(int(input("Digite a idade: ")))
-                if(opcao==00):
-                    print("\n""Aplicativo encerrado.\n")
-                    exit
-            except:
-                print("Tente novamente")
-                break
-            try:
-                resultado.append(input("Digite a Gênero: "))
-            except:
-                print("Tente novamente")
-                continue                
-            try:
-                opcao = int(input("Você costuma ouvir música por estações de rádio: \n1. Sim\n2. Não\n3. Poucas vezes \n4. Voltar\nOpção: "))
-            except:
-                print("Tente novamente")
-                continue
-            if (opcao==1):
-                resultado.append("Sim")
-            elif(opcao==2):
-                resultado.append("Não")
-            elif(opcao==3):
-                resultado.append("Poucas vezes")
-            elif(opcao==4):
-                break
-            try:
-                opcao = int(input("Você reserva um tempo do seu dia para ouvir suas músicas favoritas? \n1. Sim\n2. Não\n3. Poucas vezes \n4. Voltar\nOpção: "))
-            except:
-                print("Tente novamente")
-                continue
-            if (opcao==1):
-                resultado.append("Sim")
-            elif(opcao==2):
-                resultado.append("Não")
-            elif(opcao==3):
-                resultado.append("Poucas vezes")
-            elif(opcao==4):
-                break
-            try:
-                opcao = int(input("Você costuma ouvir gêneros musicais variádos: \n1. Sim\n2. Não\n3. Poucas vezes \n4. Voltar\nOpção: "))
-            except:
-                print("Tente novamente")
-                continue
-            if (opcao==1):
-                resultado.append("Sim")
-            elif(opcao==2):
-                resultado.append("Não")
-            elif(opcao==3):
-                resultado.append("Poucas vezes")
-            elif(opcao==4):
-                break
-            try:
-                opcao = int(input("Você usaria um app novo, no qual tocaria todas as suas músicas favoritas? \n1. Sim\n2. Não\n3. Poucas vezes \n4. Voltar\nOpção: "))
-            except:
-                print("Tente novamente")
-                continue
-            if (opcao==1):
-                resultado.append("Sim")
-            elif(opcao==2):
-                resultado.append("Não")
-            elif(opcao==3):
-                resultado.append("Poucas vezes")
-            elif(opcao==4):
-                break
+    def dados(self):
+        sim = ("Sim")
+        nao = ("Não")
+        pouco = ("Poucas vezes")
+        idade = input("Digite a idade: ")
+        genero = input("Digite o gênero: ")
+        idade = 0
+        while idade != "00":
             
-            adicionar.writerow(resultado)
-            arquivo = open("./Arquivo.csv", "a", newline="", encoding="utf-8")        
-            arquivo.close()
-            print("Arquivo criado!\n")
+            try:
+                pergunta_1 = input("Você costuma ouvir música por estações de rádio? \n1. Sim\n2. Não\n3. Poucas vezes \n4. Voltar\nOpção: ")
+            except:
+                print("Tente novamente")
+                continue
+            if (idade=="1"):
+                pergunta_1 = ("Sim")
+            elif(idade==2):
+                pergunta_1 = nao
+            elif(idade==3):
+                pergunta_1 = pouco
+                             
+            pergunta_2 = int(input("Você reserva um tempo do seu dia para ouvir suas músicas favoritas? \n1. Sim\n2. Não\n3. Poucas vezes \n4. Voltar\nOpção: "))
+            pergunta_3 = int(input("Você costuma ouvir gêneros musicais variádos? \n1. Sim\n2. Não\n3. Poucas vezes \n4. Voltar\nOpção: "))
+            pergunta_4 = int(input("Você usaria um app novo, no qual tocaria suas músicas favoritas? \n1. Sim\n2. Não\n3. Poucas vezes \n4. Voltar\nOpção: "))
+            now = datetime.datetime.now()
+            data_hora = now.strftime("%d/%m/%Y %H:%M:%S")
+            lista = [idade, genero, pergunta_1, pergunta_2, pergunta_3, pergunta_4, data_hora]
+            self.data.append(lista)
+            idade = input("Digite sua idade ou '00' para encerrar: ")
             break
-def menu():
-    import csv
-    opcao=0
-    while opcao !="00":
-        print("\n     Dificulity Tecnologia e Informação ""\n""\n""1. Iniciar questionário \n2. Exibir resultado " "\n""00. Sair")
-        try :
-            opcao = int(input("\n""Opção: "))
-        except:
-            print ("\n""Opcão inválida! Tente novamente.")
-            continue
-        if  (opcao==1):
-            entrevista() #Def para criar e editar o Arquivo CSV
-        elif(opcao==2):
-            with open("Módulo 2/Nova pasta/entrevista.csv", encoding="utf-8") as ler_arquivo:
 
-            # 2. ler a tabela
-                tabela = csv.reader(ler_arquivo, delimiter=",")
+    def salvar_arquivo_csv(self, nome):
+        with open(nome, "w", newline="") as arquivo:
+            writer = csv.writer(arquivo)
+            writer.writerow(self.cabecalho)
+            writer.writerows(self.data)
+        print(f"{len(self.data)} dado(s) coletado(s) para pesquisa. O arquivo foi salvo como {nome}")
+    
 
-                # 3. navegar pela tabela
-                for cont in tabela:        
-                    print(" | ".join(cont))
-                    break
-                    #Aqui iremos ler o arquivo CSV com As respostas
-        elif(opcao==00):
-            print("\n""Aplicativo encerrado.\n")
-            break
-menu()
