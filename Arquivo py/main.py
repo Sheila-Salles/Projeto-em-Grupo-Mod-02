eef8a53ec907e84ef0af1be7f963e44479a74d00
Rob <123372510+pezagna@users.noreply.github.com>
2023-03-07 08:43:12 -0300

    Atualizando texto.

--- Arquivo py/main.py
+++ Arquivo py/main.py
@@ -32,5 +32,5 @@ from Projeto import Entrevista
 
 perguntas = Entrevista()
 perguntas.dados()
-nome_do_arquivo = input("Digite o nome do arquivo .csv para guardar exemplo(""arquivo.csv""): ")
+nome_do_arquivo = input("Digite o nome do arquivo .csv para guardar. (ex: ""arquivo.csv""): ")
 perguntas.salvar_arquivo_csv(nome_do_arquivo)
